ed76814607e1d724c6a020edf597ae62806c4952
Ramy Ben Aroya <ramybenaroya@gmail.com>
2019-06-04 00:33:06 +0300

    refactor
    Change `App` import typo after refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './containers/App';
 import './index.css';
 import { RootContext } from './context/RootStore';
 import stores from './stores';
