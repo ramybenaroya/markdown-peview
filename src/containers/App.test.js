a599378941d9d0828ac4d87cb3281c6a75dae08c
Ramy Ben Aroya <ramybenaroya@gmail.com>
2019-06-04 02:08:22 +0300

    fix(tests)
    Fix the only sanity test for `App` component

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { RootContext } from '../context/RootStore';
+import stores from '../stores';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+	const div = document.createElement('div');
+	ReactDOM.render(
+		<RootContext.Provider value={stores}>
+			<App />
+		</RootContext.Provider>,
+		div
+	);
 });
